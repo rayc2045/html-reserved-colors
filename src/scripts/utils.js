f791a627ed528342917d88ad8d40e70210974464
rayc2045 <>
2022-04-03 17:40:48 +0800

    Optimize util functions

--- src/scripts/utils.js
+++ src/scripts/utils.js
@@ -1,15 +1,21 @@
-function getParamsByUrl(url = window.location.href) {
+export function getParamsByUrl(url = window.location.href) {
   const urlSearch = url.split('?')[1];
   const urlSearchParams = new URLSearchParams(urlSearch);
-  return Object.fromEntries(urlSearchParams.entries());
+  const entries = Object.fromEntries(urlSearchParams.entries());
+  Object.keys(entries).forEach(entry => {
+    const split = entries[entry].split(' ');
+    if (split.length === 1 && split[0] === '') return (entries[entry] = []);
+    entries[entry] = split;
+  });
+  return entries;
 }
 
-async function getData(api) {
+export async function getData(api) {
   const res = await fetch(api);
   return await res.json();
 }
 
-function getRepeatedItem(arr) {
+export function getRepeatedItem(arr) {
   const set = new Set();
   return arr.filter(item =>
     set.has(JSON.stringify(item))
@@ -18,26 +24,23 @@ function getRepeatedItem(arr) {
   );
 }
 
-function copyText(text) {
+export function getNextItem(arr, currentValue) {
+  if (!arr.includes(currentValue)) return console.log(`Can not find ${currentValue} in ${arr}`);
+  if (currentValue === arr[arr.length - 1]) return arr[0];
+  return arr[arr.indexOf(currentValue) + 1];
+}
+
+export function copyText(text) {
   navigator.clipboard.writeText(text);
 }
 
-function playAudio(audio, volume = 1) {
+export function playAudio(audio, volume = 1) {
   audio.currentTime = 0;
   audio.volume = volume;
   audio.play();
 }
 
-function audioCatch(audios, minute = 1) {
+export function audiosCatch(audios, minute = 1) {
   audios.forEach(audio => playAudio(audio, 0));
-  setTimeout(() => audioCatch(audios, minute), minute * 60_000);
-}
-
-export {
-  getParamsByUrl,
-  getData,
-  getRepeatedItem,
-  copyText,
-  playAudio,
-  audioCatch
+  setTimeout(() => audiosCatch(audios, minute), minute * 60_000);
 }
