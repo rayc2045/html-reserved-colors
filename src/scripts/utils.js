334d4367af441a484761fd7e836e2817b41ebe21
rayc2045 <>
2023-01-24 04:17:10 +0800

    Fix the function of getting query params

--- src/scripts/utils.js
+++ src/scripts/utils.js
@@ -1,13 +1,7 @@
-export function getParamsByUrl(url = window.location.href) {
+export function getQueryParams(url = window.location.href) {
   const urlSearch = url.split('?')[1];
   const urlSearchParams = new URLSearchParams(urlSearch);
-  const entries = Object.fromEntries(urlSearchParams.entries());
-  Object.keys(entries).forEach(entry => {
-    const split = entries[entry].split(' ');
-    if (split.length === 1 && split[0] === '') return (entries[entry] = []);
-    entries[entry] = split;
-  });
-  return entries;
+  return Object.fromEntries(urlSearchParams.entries());
 }
 
 export async function getData(api) {
